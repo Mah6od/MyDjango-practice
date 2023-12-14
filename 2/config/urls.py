49577819d2e1e7000832de603bcf950e7cf47662
Mahbod <mahbod.zamanpour@gmail.com>
2023-12-14 20:45:27 +0330

    add files

--- 2/config/urls.py
+++ 2/config/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from movies.views import index
+from movies.views import index, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('about/', about),
 ]
--- 2/movies/views.py
+++ 2/movies/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'movies/index.html', {})
+
+def about(request):
+    return render(request, 'movies/about.html', {})

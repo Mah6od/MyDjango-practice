5cc0f1a1dbddab2efffd03cbf9a530bc04220ed8
Mahbod <mahbod.zamanpour@gmail.com>
2023-12-15 15:38:22 +0330

    update files

--- 2/movies/views.py
+++ 2/movies/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'movies/index.html', {})
+    context = {
+        'movies': ['Home Alone', 'Hangover', 'Family Switch']
+    }
+    return render(request, 'movies/index.html', context)
 
 def about(request):
     return render(request, 'movies/about.html', {})

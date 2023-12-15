0012e91c6826518722b2fdeb34c75e8bb2444ea6
Mahbod <mahbod.zamanpour@gmail.com>
2023-12-15 15:59:17 +0330

    update file

--- 2/config/urls.py
+++ 2/config/urls.py
@@ -19,6 +19,6 @@ from movies.views import index, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('about/', about),
+    path('', index, name= "home"),
+    path('about/', about, name= "about"),
 ]

ce0c69d341ae359eebc375cc2016fe30d6f8c02d
Mahbod <mahbod.zamanpour@gmail.com>
2023-12-16 08:34:00 +0330

    update file

--- 3/job_board/models.py
+++ 3/job_board/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class JobPosting(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    description = models.CharField(max_length=100)
+    salary = models.IntegerField()

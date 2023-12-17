01b3571c18d29b11bdd6ba67e720900bead18918
Mahbod <mahbod.zamanpour@gmail.com>
2023-12-17 17:34:27 +0330

    update files

--- 3/job_board/admin.py
+++ 3/job_board/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import JobPosting
 
 # Register your models here.
+admin.site.register(JobPosting)
--- 3/job_board/models.py
+++ 3/job_board/models.py
@@ -8,6 +8,9 @@ class JobPosting(models.Model):
     salary = models.IntegerField()
     is_active = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f"{self.title} | {self.company} | Active: {self.is_active}"
+
 # CRUD operations
 # create
 # update

bc9d25adc96f21c6303872a8729ae3ef198b7a21
Mahbod <mahbod.zamanpour@gmail.com>
2023-12-18 10:04:53 +0330

    update files

--- 4/links/urls.py
+++ 4/links/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, root_link, add_link
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='home'),
+    path('<str:link_slug>/', root_link, name='root-link'),
+    path('link/create', add_link, name="create-link"),
 ]
--- 4/links/views.py
+++ 4/links/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Link
 
 # Create your views here.
 def index(request):
-    return render(request, 'links/index.html', {})
+    links = Link.objects.all()
+    context = {
+        'links': links
+    }
+    return render(request, 'links/index.html', context)
+
+def root_link(request, link_slug):
+    link = get_object_or_404(Link, slug=link_slug)
+    link.click() # this will increment the clicked field
+
+    return redirect(link.url)
+
+def add_link(request):
+    if request.method == "POST":
+        form = LinkForm(request.POST)
+        if form.is_valid():
+            # if the form is valid - then process
+            form.save()
+            return redirect(reverse('home'))
+    
+    else:
+        form = LinkForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, 'links/create.html', context)

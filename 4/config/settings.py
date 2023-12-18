8648d1da4b67909987533d98bdc60d244d97e47e
Mahbod <mahbod.zamanpour@gmail.com>
2023-12-18 10:25:56 +0330

    update files

--- 4/config/settings.py
+++ 4/config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #3rd party
+    'crispy_forms',
+    'crispy-tailwind',
+
     # local apps
     'links',
 ]
@@ -124,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CRISPY FORM
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+CRISPY_TEMPLATE_PACK = "tailwind"

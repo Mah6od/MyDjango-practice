63b58909fe9b4fbed6d5d4b9f7279263b1db38ef
Mahbod <mahbod.zamanpour@gmail.com>
2023-12-20 17:17:57 +0330

    update files

--- 5/link_plant/models.py
+++ 5/link_plant/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+# each Class is a table in our db
+# Profiles -> links
+
+class Profile(models.Model):
+    BG_CHOICES = (
+        ('blue', 'Blue'),
+        ('green', ' Green'),
+        ('yellow', 'Yellow'),
+    )
+
+    # name, slug, background color
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    bg_color = models.CharField(max_length=50, choices=BG_CHOICES)
+
+    def __str__(self):
+        return self.name
+    
+class Link(models.Model):
+    # text, url, profile
+    text = models.CharField(max_length=100)
+    url = models.URLField()
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='links')
+
+# many(profile) to many(links) & one to one & one to many

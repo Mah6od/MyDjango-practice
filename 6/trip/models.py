9482de698275f1f6e39cbed4ffbcc926366dbe7b
Mahbod <mahbod.zamanpour@gmail.com>
2023-12-24 10:23:43 +0330

    update files

--- 6/trip/models.py
+++ 6/trip/models.py
@@ -4,13 +4,13 @@ from django.core.validators import MaxValueValidator
 
 User = get_user_model()
 
-# Create your models here.
 class Trip(models.Model):
     city = models.CharField(max_length=50)
-    county = models.CharField(max_length=2) # USA = US
+    #alpha 2 country code - USA = US
+    country = models.CharField(max_length=2)
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='trips')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="trips")
 
     def __str__(self):
         return self.city
@@ -22,11 +22,12 @@ class Note(models.Model):
         ("experience", "Experience"),
         ("general", "General"),
     )
-    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='notes')
+
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name="notes")
     name = models.CharField(max_length=100)
     description = models.TextField()
     type = models.CharField(max_length=100, choices=EXCURSIONS)
-    img = models.ImageField(upload_to='notes', blank=True, null=True)
+    img = models.ImageField(upload_to="notes", blank=True, null=True)
     rating = models.PositiveSmallIntegerField(default=1, validators=[MaxValueValidator(5)])
 
     def __str__(self):
--- 6/trip/views.py
+++ 6/trip/views.py
@@ -1,10 +1,14 @@
+from typing import List
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView, DetailView, ListView, UpdateView, DeleteView
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
 from .models import Trip, Note
 
-# Create your views here.
-class homeview(TemplateView):
-    template_name =  'trip/index.html'
+class HomeView(TemplateView):
+    template_name = 'trip/index.html'
 
 def trips_list(request):
     # print(dir(request))
@@ -13,3 +17,75 @@ def trips_list(request):
         "trips": trips
     }
     return render(request, 'trip/trips_list.html', context)
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+class TripCreateView(CreateView):
+    model = Trip
+    success_url = reverse_lazy('trip-list')
+    fields = ["city", "country", "start_date", "end_date"]
+    #template named model_form.html
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+class TripDetailView(DetailView):
+    model = Trip
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        trip = context['object']
+        notes = trip.notes.all()
+        context['notes'] = notes
+        return context
+
+class TripUpdateView(UpdateView):
+    model = Trip
+    success_url = reverse_lazy('trip-list')
+    fields = ["city", "country", "start_date", "end_date"]
+    #template named model_form.html
+
+class TripDeleteView(DeleteView):
+    model = Trip
+    success_url = reverse_lazy('trip-list')
+
+class NoteListView(ListView):
+    model = Note
+
+    def get_queryset(self):
+        queryset = Note.objects.filter(trip__owner=self.request.user)
+        return queryset
+
+class NoteDetailView(DetailView):
+    model = Note
+
+class NoteCreateView(CreateView):
+    model = Note
+    success_url = reverse_lazy('note-list')
+    fields = "__all__"
+
+    def get_form(self):
+        form = super(NoteCreateView, self).get_form()
+        trips = Trip.objects.filter(owner=self.request.user)
+        form.fields['trip'].queryset = trips
+        return form
+
+class NoteUpdateView(UpdateView):
+    model = Note
+    success_url = reverse_lazy('note-list')
+    fields = "__all__"
+
+    def get_form(self):
+        form = super(NoteUpdateView, self).get_form()
+        trips = Trip.objects.filter(owner=self.request.user)
+        form.fields['trip'].queryset = trips
+        return form
+
+class NoteDeleteView(DeleteView):
+    model = Note
+    success_url = reverse_lazy('note-list')
+    #not make the template - send post requests here
